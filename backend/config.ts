7aee7fb690d6572a551a283e80e363ad73451f90
Lam Eu Ler <27113373+u018f@users.noreply.github.com>
2024-03-20 00:26:49 +0800

    handle newline at end of password

--- backend/config.ts
+++ backend/config.ts
@@ -4,4 +4,8 @@ export const API_PORT = isFinite(port_env) ? port_env : 6231
 export const MYSQL_HOST = process.env.MYSQL_HOST as string ?? 'localhost'
 export const MYSQL_DATABASE = process.env.MYSQL_DATABASE as string ?? 'sample'
 const MYSQL_ROOT_PASSWORD_FILE = process.env.MYSQL_ROOT_PASSWORD_FILE as string ?? './.secret/MYSQL_ROOT_PASSWORD'
-export const MYSQL_ROOT_PASSWORD = await Bun.file(MYSQL_ROOT_PASSWORD_FILE).text()
+const result = (await Bun.file(MYSQL_ROOT_PASSWORD_FILE).text()).split('\n')
+export const MYSQL_ROOT_PASSWORD = result.shift()
+if (result.join('').length > 0) {
+    throw new Error('MYSQL_ROOT_PASSWORD can only be 1 line')
+}
